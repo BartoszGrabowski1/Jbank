8d663e394571cc5e51b24efad083c1156bf71298
BartoszGrabowski1 <heksargonh@gmail.com>
2023-03-07 12:36:41 +0100

    first commit

---
+++ src/test/java/com/jbanksite/Jbank/JbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jbanksite.Jbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
